43b839a1152aa2270715e945f4891f0e6c368409
Philip Grohe <phil.grohe@gmail.com>
2011-02-09 22:20:38 -0500

    Continued building up DataSource interface and the DummyDataSource mock class that implements it.  Wrote some test code for DummyDataSource in main.cpp

--- src/dummydatasource.h
+++ src/dummydatasource.h
@@ -4,18 +4,25 @@
 //dummydatasource.h
 //A mock/dummy class that implements the DataSource interface
 
+
+#define NUM_CONTACTS 5
+
+#include <vector>
 #include "datasource.h"
 #include "contact.h"
+
+
 
 class DummyDataSource : DataSource
 {
     private:
-        Contact data;
+        std::vector<Contact> recordList;
     public:
         DummyDataSource();  
         ~DummyDataSource() { };
         
         virtual Contact getContact(int id);
+        virtual ContactRecordSet getAllContacts();
         virtual bool addContact(const Contact& c);
         virtual bool updateContact(int id, const Contact& c);
         virtual bool deleteContact(int id);
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
 #include <string>
 #include "dummydatasource.h"
+#include "contact.h"
+
+
+//Testing code for the DataSource interface as implemented by the DummyDataSource class
 
 int main()
 {
     DummyDataSource myDataSource;
 
-    Contact x = myDataSource.getContact(1);
+    ContactRecordSet rs = myDataSource.getAllContacts();
 
-    std::cout << x.firstName << std::endl;
-    std::cout << x.lastName << std::endl;
-    std::cout << x.phoneNumber << std::endl;
+    ContactRecordSet::const_iterator it;
+
+    for(it=rs.begin(); it != rs.end(); it++)
+    {
+        std::cout << it->firstName << std::endl;
+    }
+
+    std::cout << "3rd Contact's first name is: " <<myDataSource.getContact(3).firstName << std::endl;
+
+    Contact c = {"Joe", "Blow", "555-5555", "123 1st St", "asdf@abc.def"};
+
+    myDataSource.updateContact(5, c);
+    std::cout << "5th Contact's first name is: " << myDataSource.getContact(5).firstName << std::endl;
+    
+    myDataSource.deleteContact(5);
+
+    rs = myDataSource.getAllContacts();
+
+    for(it=rs.begin(); it != rs.end(); it++)
+    {
+        std::cout << it->firstName << std::endl;
+    }
+
 
     return 0;
 }
