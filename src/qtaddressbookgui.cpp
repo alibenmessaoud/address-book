bfa4a3bd10943e76091849d8b7fc4645af553c54
Philip Grohe <phil.grohe@gmail.com>
2011-03-13 18:24:06 -0400

    Missed file qtaddressbookgui.cpp in last commit.

---
+++ src/qtaddressbookgui.cpp
@@ -0,0 +1,29 @@
+#include "qtaddressbookgui.h"
+#include "addressbookcontroller.h"
+
+QtAddressBookGUI::QtAddressBookGUI(AddressBookController & controller) : AddressBookUI(controller)
+{
+    //create main window
+    appWindow = new QtMainWindow(appController);
+
+}
+
+bool QtAddressBookGUI::updateData(void)
+{
+    appWindow->updateData(); 
+    return true;
+}
+
+
+bool QtAddressBookGUI::show(void)
+{
+    appWindow->show();
+    return true;
+}
+
+QtAddressBookGUI::~QtAddressBookGUI()
+{
+    //delete main window
+    delete appWindow;
+}
+
